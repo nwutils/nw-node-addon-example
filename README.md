21e7f7d259824e109732f527c9e439fa53af16f2
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2023-12-24 23:22:14 +0530

    chore: add to git

---
+++ README.md
@@ -0,0 +1,10 @@
+# nw-node-addon-gyp-example
+
+Build Node Addon using `node-gyp`. NW.js Node headers are downloaded and passed to `node-gyp` via the `--nodedir` parameter.
+
+## Getting Started
+
+1. Clone repo
+1. `npm i` to install Node modules
+1. `npm run gyp` to download Node headers and build Node Native Addon
+1. `npm run nw` to run NW.js app.
---
+++ index.mjs
@@ -0,0 +1,64 @@
+import child_process from "node:child_process";
+import fs from "node:fs";
+import https from "node:https";
+import path from "node:path";
+import process from "node:process";
+
+import tar from "tar";
+
+import nodeManifest from "./package.json" assert { type: "json"};
+
+const nwVersion = nodeManifest.devDependencies.nw.slice(1);
+const headersUrl = `https://dl.nwjs.io/v${nwVersion}/nw-headers-v${nwVersion}.tar.gz`;
+const headersPath = path.resolve(process.cwd(), `nw-headers-v${nwVersion}.tar.gz`);
+
+await main();
+
+async function downloadHeaders() {
+    const writeStream = fs.createWriteStream(headersPath);
+
+    return new Promise((resolve, reject) => {
+        https.get(headersUrl, (res) => {
+
+            let chunks = 0;
+
+            res.on("data", (chunk) => {
+                chunks += chunk;
+            });
+
+            res.on("end", () => {
+                resolve(chunks);
+            });
+
+            res.on("error", (error) => {
+                reject(error);
+            });
+
+            res.pipe(writeStream);
+        });
+    });
+}
+
+async function decompressHeaders() {
+    await fs.promises.rm('./node', { recursive: true, force: true });
+
+    await tar.extract({
+        file: headersPath,
+        C: process.cwd(),
+    });
+}
+
+async function rebuild() {
+    child_process.exec(`node-gyp rebuild --nodedir=${path.resolve('node')}`);
+}
+
+async function main() {
+
+    if (fs.existsSync(headersPath) === false) {
+        await downloadHeaders();
+    }
+
+    await decompressHeaders();
+
+    rebuild();
+}
