407884ff25bc1835d76a5a5350bb534f73c98909
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-05-04 23:00:55 +0530

    chore: update native addond types

--- hello.cc
+++ hello.cc
@@ -1,3 +1,4 @@
+// hello.cc
 #include <node.h>
 
 namespace demo {
@@ -5,6 +6,7 @@ namespace demo {
 using v8::FunctionCallbackInfo;
 using v8::Isolate;
 using v8::Local;
+using v8::NewStringType;
 using v8::Object;
 using v8::String;
 using v8::Value;
@@ -12,13 +14,13 @@ using v8::Value;
 void Method(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
   args.GetReturnValue().Set(String::NewFromUtf8(
-      isolate, "world").ToLocalChecked());
+      isolate, "world", NewStringType::kNormal).ToLocalChecked());
 }
 
 void Initialize(Local<Object> exports) {
   NODE_SET_METHOD(exports, "hello", Method);
 }
 
-NODE_MODULE(DEMO, Initialize)
+NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
 
 }  // namespace demo 
