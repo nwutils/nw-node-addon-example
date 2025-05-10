0a5a2493fa37c825fb7cade43883424d84ece39a
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-05-10 15:06:19 +0530

    chore: upgrade addon type

--- hello.cc
+++ hello.cc
@@ -5,6 +5,7 @@ namespace demo {
 using v8::FunctionCallbackInfo;
 using v8::Isolate;
 using v8::Local;
+using v8::NewStringType;
 using v8::Object;
 using v8::String;
 using v8::Value;
@@ -12,13 +13,13 @@ using v8::Value;
 void Method(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
   args.GetReturnValue().Set(String::NewFromUtf8(
-      isolate, "world").ToLocalChecked());
+      isolate, "world", NewStringType::kNormal).ToLocalChecked());
 }
 
 void Initialize(Local<Object> exports) {
   NODE_SET_METHOD(exports, "hello", Method);
 }
 
-NODE_MODULE(DEMO, Initialize)
+NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
 
-}  // namespace demo 
+}  // namespace demo
